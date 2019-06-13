02e3617bccb3b701655b45ec4946ed4e3ddd40d1
nuwanKodi3 <nuwankodi3@gmail.com>
2019-06-13 23:46:44 +0530

    creating hello world library

---
+++ helloworldlibrary/src/main/java/com/example/helloworldlibrary/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworldlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class HelloWorld {
+
+    public void printHelloWorld(Context context,String message){
+
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+}
